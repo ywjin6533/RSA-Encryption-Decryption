b785fe9375370af08c1e3c8dea9028464cb6ff2f
unknown <ywjin6533@gmail.com>
2024-11-05 20:31:21 +0900

    added prime_in_range function

---
+++ required/prime.py
@@ -0,0 +1,18 @@
+def primes_in_range(lower=20000, upper=30000):
+    if upper < 2:
+        return []
+
+    numbers = [True] * (upper + 1)
+    numbers[0:2] = [False, False]  # 0과 1은 소수가 아님
+
+    for num in range(2, int(upper ** 0.5) + 1):
+        if numbers[num]:
+            for multiple in range(num * num, upper + 1, num):
+                numbers[multiple] = False
+
+    primes = [num for num in range(lower, upper + 1) if numbers[num]]
+    return primes
+
+if __name__ == '__main__':
+    primes_between_20000_and_30000 = primes_in_range()
+    print(primes_between_20000_and_30000)
